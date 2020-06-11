9a10ce5a10587d8a760072d5338bc2c63e609eec
hongdy <h44574218@sina.com>
2020-06-11 23:14:40 +0800

    japtest new

---
+++ src/main/java/com/hongdy/code/Main.java
@@ -0,0 +1,35 @@
+package com.hongdy.code;
+
+import com.hongdy.code.model.PersonInformation;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class Main {
+
+    private static final String PERSISTENCE_UNIT_NAME = "EmployeeService";
+
+    private static EntityManagerFactory factory;
+
+    public static void main(String[] args) {
+//        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+//        EntityManager em = factory.createEntityManager();
+//        em.getTransaction().begin();
+//        PersonInformation person = new PersonInformation();
+//        person.setId(1);
+//        person.setAge(30);
+//        person.setName("fred");
+//        em.persist(person);
+//        em.getTransaction().commit();
+//        em.close();
+
+        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        EntityManager em = factory.createEntityManager();
+        Query query = em.createQuery("SELECT pi.age FROM PersonInformation pi where pi.id=?1");
+        query.setParameter(1,1);
+        Object object = query.getSingleResult();
+
+    }
+}
